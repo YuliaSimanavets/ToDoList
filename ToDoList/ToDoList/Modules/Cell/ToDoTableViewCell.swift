bba28a48c22d0fefcbb3da3f9e0c14861358cea4
YuliaSimanavets <mrs.simonovets@gmail.com>
2023-11-02 15:37:11 +0100

    added checkBox to cell [ToDoTableViewCell]

--- ToDoList/ToDoList/Modules/Cell/ToDoTableViewCell.swift
+++ ToDoList/ToDoList/Modules/Cell/ToDoTableViewCell.swift
@@ -10,21 +10,14 @@ import UIKit
 
 struct ToDoViewModel {
     let toDoTaskText: String
-//    let doneMark: Bool
+    let checked: Bool
 }
 
 class ToDoTableViewCell: UITableViewCell {
-    
+
     static var identifier: String {
         return String(describing: ToDoTableViewCell.self)
     }
-    
-    private let cellView: UIView = {
-        let view = UIView()
-        view.backgroundColor = .white
-        view.translatesAutoresizingMaskIntoConstraints = false
-        return view
-    }()
 
     private let taskLabel: UILabel = {
         let label = UILabel()
@@ -34,59 +27,62 @@ class ToDoTableViewCell: UITableViewCell {
         label.translatesAutoresizingMaskIntoConstraints = false
         return label
     }()
-    
-    private let doneMarkImageView: UIImageView = {
-        let imageView = UIImageView()
-        imageView.image = UIImage(systemName: "circle")
-//        imageView.image = UIImage(systemName: "checkmark.circle.fill")
-        imageView.tintColor = .systemGreen
-        imageView.translatesAutoresizingMaskIntoConstraints = false
-        return imageView
-    }()
-    
+
+    private let checkBox = Checkbox()
+
     override init(style: UITableViewCell.CellStyle, reuseIdentifier: String?) {
         super.init(style: style, reuseIdentifier: reuseIdentifier)
         setupView()
     }
-    
+
     required init?(coder: NSCoder) {
         fatalError("init(coder:) has not been implemented")
     }
-    
+
     private func setupView() {
-        backgroundColor = .clear
-        addSubview(cellView)
-        cellView.addSubview(taskLabel)
-        cellView.addSubview(doneMarkImageView)
-        
+        backgroundColor = .white
+        addSubview(taskLabel)
+        addSubview(checkBox)
+
+        checkBox.addTarget(self, action: #selector(checked), for: .touchUpInside)
+
         NSLayoutConstraint.activate([
-            cellView.topAnchor.constraint(equalTo: topAnchor),
-            cellView.leadingAnchor.constraint(equalTo: leadingAnchor),
-            cellView.trailingAnchor.constraint(equalTo: trailingAnchor),
-            cellView.bottomAnchor.constraint(equalTo: bottomAnchor),
-            cellView.heightAnchor.constraint(equalToConstant: 50),
-            
-            taskLabel.centerXAnchor.constraint(equalTo: cellView.centerXAnchor),
-            taskLabel.centerYAnchor.constraint(equalTo: cellView.centerYAnchor),
-            taskLabel.leadingAnchor.constraint(equalTo: cellView.leadingAnchor, constant: 10),
-            taskLabel.trailingAnchor.constraint(equalTo: doneMarkImageView.leadingAnchor, constant: -10),
-            taskLabel.topAnchor.constraint(equalTo: cellView.topAnchor, constant: 10),
-            taskLabel.bottomAnchor.constraint(equalTo: cellView.bottomAnchor, constant: -10),
-            
-            doneMarkImageView.centerYAnchor.constraint(equalTo: taskLabel.centerYAnchor),
-            doneMarkImageView.trailingAnchor.constraint(equalTo: cellView.trailingAnchor, constant: -10),
-            doneMarkImageView.heightAnchor.constraint(equalToConstant: 35),
-            doneMarkImageView.widthAnchor.constraint(equalToConstant: 35)
+            taskLabel.leadingAnchor.constraint(equalTo: leadingAnchor, constant: 10),
+            taskLabel.trailingAnchor.constraint(equalTo: centerXAnchor),
+            taskLabel.topAnchor.constraint(equalTo: topAnchor, constant: 15),
+            taskLabel.bottomAnchor.constraint(equalTo: bottomAnchor, constant: -15),
+            taskLabel.heightAnchor.constraint(equalToConstant: 20),
+
+            checkBox.centerYAnchor.constraint(equalTo: taskLabel.centerYAnchor),
+            checkBox.leadingAnchor.constraint(equalTo: centerXAnchor, constant: 10),
+            checkBox.heightAnchor.constraint(equalToConstant: 50),
+            checkBox.widthAnchor.constraint(equalToConstant: 50)
         ])
     }
 
     func set(_ data: ToDoViewModel) {
         taskLabel.text = data.toDoTaskText
-//        if data.doneMark {
-//            doneMarkImageView.image = UIImage(systemName: "checkmark.circle.fill")
-//            doneMarkImageView.tintColor = .systemGreen
-//        } else {
-//            doneMarkImageView.image = UIImage(systemName: "circle")
-//        }
+        set(checked: data.checked)
+        updateChecked()
+    }
+
+    private func updateChecked() {
+        let attributedString = NSMutableAttributedString(string: taskLabel.text!)
+
+        if checkBox.checked {
+            attributedString.addAttribute(.strikethroughStyle, value: 2, range: NSMakeRange(0, attributedString.length-1))
+        } else {
+            attributedString.removeAttribute(.strikethroughStyle, range: NSMakeRange(0, attributedString.length-1))
+        }
+    }
+
+    @objc
+    func checked() {
+        updateChecked()
+    }
+    
+    func set(checked: Bool) {
+        checkBox.checked = checked
+        updateChecked()
     }
 }
